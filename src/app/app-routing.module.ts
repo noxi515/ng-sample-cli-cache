e330111210416aa6468fb856a9f2d69af9ccae7d
noxi515 <tzo.ppl@gmail.com>
2022-02-11 17:44:31 +0900

    ページ追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'hoge', loadChildren: () => import('./hoge/hoge.module').then(m => m.HogeModule) }, { path: 'fuga', loadChildren: () => import('./fuga/fuga.module').then(m => m.FugaModule) }, { path: 'piyo', loadChildren: () => import('./piyo/piyo.module').then(m => m.PiyoModule) }];
+const routes: Routes = [{ path: 'hoge', loadChildren: () => import('./hoge/hoge.module').then(m => m.HogeModule) }, { path: 'fuga', loadChildren: () => import('./fuga/fuga.module').then(m => m.FugaModule) }, { path: 'piyo', loadChildren: () => import('./piyo/piyo.module').then(m => m.PiyoModule) }, { path: 'bar', loadChildren: () => import('./bar/bar.module').then(m => m.BarModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
